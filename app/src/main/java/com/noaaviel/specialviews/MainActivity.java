839f6c96d1a3189109c45ff76273c95a1c9e39b7
noaavielinmanage <119415508+noaavielinmanage@users.noreply.github.com>
2023-07-15 20:41:15 +0300

    SpannableHelper updated + example app

--- app/src/main/java/com/noaaviel/specialviews/MainActivity.java
+++ app/src/main/java/com/noaaviel/specialviews/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.text.style.TypefaceSpan;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,7 +21,7 @@ import com.noaaviel.spannablehelper.SpannableHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView textView, text1, text2,text3,text4,text5,text6;
     SpannableString spannableString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void initViews(){
         textView = findViewById(R.id.tvNoaExample);
+        text1 = findViewById(R.id.tvNoaExample1);
+        text2 = findViewById(R.id.tvNoaExample2);
+        text3 = findViewById(R.id.tvNoaExample3);
+        text4 = findViewById(R.id.tvNoaExample4);
+        text5 = findViewById(R.id.tvNoaExample5);
+        text6 = findViewById(R.id.tvNoaExample6);
+
         String text = (String) textView.getText();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             spannableString = new SpannableString(textView.getText().toString());
             spannableString = SpannableHelper.showTextPartlyBold(text,"Noa",getColor(com.noaaviel.spannablehelper.R.color.purple_700));
         }
         textView.setText(spannableString);
+        SpannableHelper.addStrikethroughText(text1,"noa");
+        SpannableHelper.addUnderlineText(text2,"noa");
+        SpannableHelper.changeFontSize(text3,"noa",18);
+        SpannableHelper.changeFontColor(text4,"noa",R.color.teal_200);
+        SpannableHelper.changeBackgroundColor(text5,"noa",R.color.color);
     }
 }
--- spannablehelper/src/main/java/com/noaaviel/spannablehelper/SpannableHelper.java
+++ spannablehelper/src/main/java/com/noaaviel/spannablehelper/SpannableHelper.java
@@ -8,9 +8,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
 import android.text.style.TypefaceSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Pair;
+import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -54,4 +60,89 @@ public class SpannableHelper extends AppCompatActivity {
             return null;
         }
     }
+
+    public static void addStrikethroughText(TextView textView, String patternText) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        Pair<Integer, Integer> indices = getPatternIndices(patternText.toLowerCase(), textView.getText().toString().toLowerCase());
+        if (indices != null) {
+            spannableString.setSpan(
+                    new StrikethroughSpan(),
+                    indices.first,
+                    indices.second,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            textView.setText(spannableString);
+        }
+    }
+
+    public static void addUnderlineText(TextView textView, String patternText) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        Pair<Integer, Integer> indices = getPatternIndices(patternText.toLowerCase(), textView.getText().toString().toLowerCase());
+        if (indices != null) {
+            spannableString.setSpan(
+                    new UnderlineSpan(),
+                    indices.first,
+                    indices.second,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            textView.setText(spannableString);
+        }
+    }
+
+    public static void changeFontSize(TextView textView, String patternText, int newSize) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        Pair<Integer, Integer> indices = getPatternIndices(patternText.toLowerCase(), textView.getText().toString().toLowerCase());
+        if (indices != null) {
+            spannableString.setSpan(
+                    new AbsoluteSizeSpan(newSize, true),
+                    indices.first,
+                    indices.second,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            textView.setText(spannableString);
+        }
+    }
+
+    public static void changeFontColor(TextView textView, String patternText, int newColor) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        Pair<Integer, Integer> indices = getPatternIndices(patternText.toLowerCase(), textView.getText().toString().toLowerCase());
+        if (indices != null) {
+            spannableString.setSpan(
+                    new ForegroundColorSpan(newColor),
+                    indices.first,
+                    indices.second,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            textView.setText(spannableString);
+        }
+    }
+
+    public static void changeBackgroundColor(TextView textView, String patternText, int newColor) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        Pair<Integer, Integer> indices = getPatternIndices(patternText.toLowerCase(), textView.getText().toString().toLowerCase());
+        if (indices != null) {
+            spannableString.setSpan(
+                    new BackgroundColorSpan(newColor),
+                    indices.first,
+                    indices.second,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            textView.setText(spannableString);
+        }
+    }
+
+    public static void changeTextStyle(TextView textView, String patternText, int style) {
+        SpannableString spannableString = new SpannableString(textView.getText());
+        Pair<Integer, Integer> indices = getPatternIndices(patternText.toLowerCase(), textView.getText().toString().toLowerCase());
+        if (indices != null) {
+            spannableString.setSpan(
+                    new StyleSpan(style),
+                    indices.first,
+                    indices.second,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            textView.setText(spannableString);
+        }
+    }
+
 }
